1518a063e112f2f4d46111a3bd538c8234da5546
danirudayarathna <danirudayarathna2002@gmail.com>
2025-11-16 15:05:10 +0530

    first commit

---
+++ lib/util/todo_tile.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class ToDoTile extends StatelessWidget {
+  const ToDoTile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(25.0),
+      child: Container(
+        child: Text("Make tutorial"),
+        decoration: BoxDecoration(color: Colors.yellow),
+      ),
+    );
+  }
+}
